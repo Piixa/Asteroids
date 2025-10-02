f1482f243adf4e1e0c1560011be71705ddb8a1a3
Elumi <sean@v6e.co.uk>
2025-10-02 16:02:49 +0100

    B: Player class

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import pygame
 from constants import *
+from circleshape import *
+from player import *
 
 def main():
     pygame.init()
@@ -7,12 +9,24 @@ def main():
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+    clock = pygame.time.Clock()
+    dt = 0
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+    player = Player((SCREEN_WIDTH/2),(SCREEN_HEIGHT/2))
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         pygame.Surface.fill(screen,(0,0,0))
+        updatable.update(dt)
+        for entity in drawable:
+            entity.draw(screen)
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
+        
+
 
 if __name__ == "__main__":
     main()
